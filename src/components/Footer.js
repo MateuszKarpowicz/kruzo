50325624c3cc3083419fc621fa88c780d7925666
MateuszKarpowicz <mateuszkarpowicz@gmail.com>
2023-05-18 22:46:09 +0200

    FOOTER IS WORKING

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,6 +10,8 @@ import phoneNavbar from '../assets/navbar/phone-navbar.png'
 import mailNavbar from '../assets/navbar/mail-navbar.png'
 import footerTexture from '../assets/footer/footer-texture.png'
 import apartmentsLogo from '../assets/footer/apartments-logo.png'
+import { useTranslation } from 'react-i18next';
+import { Link } from "react-router-dom";
 
 function Footer() {
 
@@ -19,6 +21,14 @@ function Footer() {
 
     const handleButtonFb = () => {
         window.open("https://www.facebook.com/Crusoeapartamenty/?locale=pl_PL", "_blank");
+    }
+
+    const handleButtonMail = () => {
+        window.location.href = "mailto:info@crusoe.info";
+    }
+
+    const handleButtonPhone = () => {
+        window.location.href = "tel:+48885398888";
     }
 
     return (
@@ -47,32 +57,35 @@ function Footer() {
                     </div>
                     <div className="footer-contact">
                         <div className="footer-contact-text-box">
-                        <div className="footer-contact-header">
-                        <span>Kontakt</span>
-                        </div>
-                        <div className="footer-contact-phone">
-                        <img src={phoneNavbar} alt="phone-navbar" />
-                        <span>+48 885 398 888</span>
-                        </div>
-                        <div className="footer-contact-mail">
-                        <img src={mailNavbar} alt="mail-navbar" />
-                        <span>info@crusoe.info</span>
-                        </div>
+                            <div className="footer-contact-header">
+                                <span>Kontakt</span>
+                            </div>
+                            <div className="footer-contact-phone" onClick={handleButtonPhone}>
+                                <img src={phoneNavbar} alt="phone-navbar" />
+                                <span>+48 885 398 888</span>
+                            </div>
+                            <div className="footer-contact-mail" onClick={handleButtonMail}>
+                                <img src={mailNavbar} alt="mail-navbar" />
+                                <span>info@crusoe.info</span>
+                            </div>
                         </div>
                     </div>
+
                     <div className="footer-social-links">
                         <div className="footer-social-links-text-box">
                         <div className="footer-social-links-header">
                         <span>Media</span>
                         </div>
-                        <div className="footer-social-links-insta">
-                        <img src={instaNavbar} alt="insta-navbar" onClick={handleButtonInsta} />
-                        <span>Instagram</span>
-                        </div>
-                        <div className="footer-social-links-fb">
-                        <img src={fbNavbar} alt="fb-navbar" onClick={handleButtonFb} />
-                        <span>Facebook</span>
-                        </div>
+                            <div className="footer-social-links-insta" onClick={handleButtonInsta}>
+                                <img src={instaNavbar} alt="insta-navbar" />
+                                <span>Instagram</span>
+                            </div>
+
+                            <div className="footer-social-links-fb" onClick={handleButtonFb}>
+                                <img src={fbNavbar} alt="fb-navbar" />
+                                <span>Facebook</span>
+                            </div>
+
                     </div>
                     </div>
                     <div className="footer-apartments">
@@ -81,11 +94,11 @@ function Footer() {
                         </div>
                         <div className="footer-apartments-zakopane">
                             <img className="apartments-logo-zakopane" src={apartmentsLogo} alt="apartments-logo" />
-                            <span>Zakopane</span>
+                            <Link to="/zakopane"><span>Zakopane</span></Link>
                         </div>
                         <div className="footer-apartments-primosten">
                             <img className="apartments-logo-primosten" src={apartmentsLogo} alt="apartments-logo" />
-                        <span>Primosten</span>
+                            <Link to="/primosten"><span>Primosten</span></Link>
                         </div>
                     </div>
                 </div>
