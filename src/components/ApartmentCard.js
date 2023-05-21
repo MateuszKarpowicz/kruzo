d7a0be80771be2590a5f238c6a24ca6fcf8509f7
MateuszKarpowicz <mateuszkarpowicz@gmail.com>
2023-05-22 00:57:42 +0200

    SOME UPDATES TO APARTMENT CARD STYLING

--- src/components/ApartmentCard.js
+++ src/components/ApartmentCard.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Image from '../assets/apartment/Image1.jpg'
-import Highlights from "./Highlights";
+import HighlightsApartmentCard from "./HighlightsApartmentCard";
+import ApartmentCardGraphic from "../assets/apartmentcard/apartments-card-texture-down.png";
+
 import "../scss/ApartmentCard.scss";
 
 const ApartmentCard = () => {
@@ -19,17 +21,24 @@ const ApartmentCard = () => {
                 <div className="apartment-card-image">
                 <img src={Image} alt="apartment" />
                     <div className="apartment-card-features-txt">
-                        <p>{t('apartmentCard.features')}</p>
+                        <p>{t('apartmentCard.feature1')}</p>
+                        <p>{t('apartmentCard.feature2')}</p>
+                        <p>{t('apartmentCard.feature3')}</p>
+                        <p>{t('apartmentCard.feature4')}</p>
+                        <p>{t('apartmentCard.feature5')}</p>
+                        <p>{t('apartmentCard.feature6')}</p>
+                        <p>{t('apartmentCard.feature7')}</p>
                     </div>
                 </div>
             </div>
             <div className="apartment-card-content-down">
                 <div className="apartment-card-content-highlights">
-                    <Highlights />
+                    <HighlightsApartmentCard />
                 </div>
-                <div className="apartment-card-content-description">
-                    <p>{t('apartmentCard.description')}</p>
+                <div className="apartment-card-graphic">
+                    <img src={ApartmentCardGraphic} alt="apartment-card-graphic" />
                 </div>
+
             </div>
         </div>
 </div>
