552510915c2bab24c3fa628c8d186772cefdeace
Duckjaniels <maksym73@gmail.com>
2023-05-21 20:30:56 +0200

    Addind css ContactPage

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,6 +3,8 @@ import Layout from "../components/Layout";
 import LefletMap from "../components/LeafletMap";
 import ContactsClipBoardComponent from "../components/ContactsClipBoardComponent";
 import BootstrapMailToForm from "../components/BootstrapMailToForm";
+import '../global.css';
+import './ContactPage.scss'
 
 const ContactPage = () => {
 
