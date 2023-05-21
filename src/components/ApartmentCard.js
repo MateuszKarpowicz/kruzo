f03eacd54421cf420d783f77e6566a3e0697de0c
MateuszKarpowicz <mateuszkarpowicz@gmail.com>
2023-05-21 21:35:37 +0200

    APARTMENT CARD ADDED. NOT STYLED

--- src/components/ApartmentCard.js
+++ src/components/ApartmentCard.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Image from '../assets/apartment/Image1.jpg'
+import Highlights from "./Highlights";
+import "../scss/ApartmentCard.scss";
 
-const ApartmentCard = ({ image, titleKey, descriptionKey, price, size }) => {
+const ApartmentCard = () => {
     const { t } = useTranslation();
 
     return (
         <div className="apartment-card">
+            <div className="apartment-card-title">
+                <h3>{t('apartmentCard.title')}</h3>
+                <p>{t('apartmentCard.titlePS')}</p>
 
-            <div className="apartment-info">
-                <h3>{t(titleKey)}</h3>
-                <p>{t(descriptionKey)}</p>
-                <p className="apartment-price">{price}</p>
-                <p className="apartment-size">{size} m²</p>
             </div>
-            <div className="apartment-image">
-                <img src={image} alt={t(titleKey)} />
+            <div className="apartment-card-content">
+                <div className="apartment-card-content-up">
+                <div className="apartment-card-image">
+                <img src={Image} alt="apartment" />
+                    <div className="apartment-card-features-txt">
+                        <p>{t('apartmentCard.features')}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="apartment-card-content-down">
+                <div className="apartment-card-content-highlights">
+                    <Highlights />
+                </div>
+                <div className="apartment-card-content-description">
+                    <p>{t('apartmentCard.description')}</p>
+                </div>
             </div>
         </div>
+</div>
     );
 };
 
