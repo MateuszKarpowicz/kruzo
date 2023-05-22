b00f8c8b34f1129712b85a3d2eb618a0baf189c5
Duckjaniels <maksym73@gmail.com>
2023-05-22 16:17:22 +0200

    changing awards icons

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../scss/Awards.scss';
-import bookingIcon from '../assets/awards/booking-icon.png';
-import tripIcon from '../assets/awards/trip-icon.png';
+import bookingIcon from '../assets/awards/booking-icon.webp';
+import tripIcon from '../assets/awards/trip-icon.webp';
 
 const Awards = () => {
 
