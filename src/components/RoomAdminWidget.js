0570d5bc34d3362e927f3f25e6467ae79151265f
Duckjaniels <maksym73@gmail.com>
2023-05-24 23:37:12 +0200

    ReservationPage and widgets styling

--- src/components/RoomAdminWidget.js
+++ src/components/RoomAdminWidget.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-
+import '../scss/RoomAdminWidget.scss'
 function BookingWidget() {
     const iframeRef = useRef(null);
 
--- src/pages/ReservationPage.js
+++ src/pages/ReservationPage.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Layout from "../components/Layout";
 import BookingWidget from '../components/RoomAdminWidget';
 import BookingCalendar from '../components/RoomAdminCalendar';
-
+import './ReservationPage.scss';
 const ReservationPage = () => {
     return (
         <Layout>
-            <BookingWidget />
-            <BookingCalendar />
+            <div className='booking-container'>
+                <div className='booking-widget'>
+                <BookingWidget />
+                </div>
+                <div className='booking-calendar'>
+                <BookingCalendar />
+                </div>
+            </div>
+            
         </Layout>
     );
 };
