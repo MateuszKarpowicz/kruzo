33cdb0e01d311b596b3b74ad9c588119fad6d46e
Duckjaniels <maksym73@gmail.com>
2023-05-25 07:43:49 +0200

    Adding some animations to homepage

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../scss/Awards.scss';
 import bookingIcon from '../assets/awards/booking-icon.webp';
 import tripIcon from '../assets/awards/trip-icon.webp';
 
 const Awards = () => {
+  const [showIcons, setShowIcons] = useState(false);
+  const awardsRef = useRef(null);
+
+  const handleScroll = () => {
+    const element = awardsRef.current;
+    const distanceFromTop = element.getBoundingClientRect().top;
+
+    if (distanceFromTop - window.innerHeight + 100 <= 0) {
+      setShowIcons(true);
+    } else {
+      setShowIcons(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <div className ="awards-container">
-      <div className="awards-icons">
+    <div className="awards-container" ref={awardsRef}>
+      <div className={`awards-icons ${showIcons ? 'show' : ''}`}>
         <div className="awards-trip" style={{ backgroundImage: `url(${tripIcon})` }}></div>
         <div className="awards-booking" style={{ backgroundImage: `url(${bookingIcon})` }}></div>
       </div>
     </div>
-    
-          );
-        }
-        
-        export default Awards;
+  );
+};
+
+export default Awards;
--- src/components/Together.js
+++ src/components/Together.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import "../scss/Together.scss"
-import InstagramWidget from "./InstagramWidget";
+import '../scss/Together.scss';
+import InstagramWidget from './InstagramWidget';
 
 const Together = () => {
   const { t } = useTranslation();
+  const [showTextContainer, setShowTextContainer] = useState(false);
+  const [showPhotoContainer, setShowPhotoContainer] = useState(false);
+  const togetherRef = useRef(null);
+
+  const handleScroll = () => {
+    const element = togetherRef.current;
+    const distanceFromTop = element.getBoundingClientRect().top;
+
+    if (distanceFromTop - window.innerHeight + 100 <= 0) {
+      setShowTextContainer(true);
+      setShowPhotoContainer(true);
+    } else {
+      setShowTextContainer(false);
+      setShowPhotoContainer(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-  <div className="together-container">
+    <div className="together-container" ref={togetherRef}>
+      <div className={`together-text_container ${showTextContainer ? 'show' : ''}`}>
+        <p>{t('togetherPage.description')}</p>
+      </div>
 
-    <div className="together-text_container">
-      <p>{t('togetherPage.description')}</p>
-    </div>
-
-    <div className="together-photo_container">
+      <div className={`together-photo_container ${showPhotoContainer ? 'show' : ''}`}>
         <InstagramWidget />
+      </div>
     </div>
-  </div>
   );
-}
+};
+
 export default Together;
