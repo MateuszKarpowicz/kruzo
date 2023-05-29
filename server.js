3323c037f0ea2c6dd4c0cb0584d7b9350e383c7e
Duckjaniels <maksym73@gmail.com>
2023-05-29 13:37:26 +0200

    working on path and improve HomePage styling

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const app = express();
 
-// Konfiguracje aplikacji Express
+// Inne konfiguracje aplikacji Express
+
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'), function(err) {
     if (err) {
       res.status(500).send(err);
     }
